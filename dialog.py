2d81e3d7c4e6e193467fb0a1ddbf2bde474cf945
Krutenko <krutenko.vlad@gmail.com>
2021-03-18 23:24:10 +0300

    Масштабирование окна

--- dialog.py
+++ dialog.py
@@ -3,25 +3,27 @@ import sys
 from PyQt5.QtCore import (
     QAbstractListModel,
     QMargins,
-    QPoint,
     Qt,
     QSize,
     QRect,
+    QPoint,
+    QRectF,
     QEvent,
 )
 from PyQt5.QtGui import (
     QColor,
+    QPainter,
     QPixmap,
     QIcon,
-    QPainter,
     QFont,
-    QFontDatabase,
     QFontMetrics,
+    QFontDatabase,
     QKeyEvent,
+    QTextDocument,
+    QTextOption,
 )
 from PyQt5.QtWidgets import (
     QApplication,
-    QTextEdit,
     QListView,
     QMainWindow,
     QVBoxLayout,
@@ -29,6 +31,7 @@ from PyQt5.QtWidgets import (
     QWidget,
     QStyledItemDelegate,
     QAbstractButton,
+    QTextEdit,
 )
 
 import resources
@@ -52,6 +55,15 @@ class Defines():
     SEND_HOVER_IMG = ":/img/send2.png"
     SEND_PRESS_IMG = ":/img/send3.png"
 
+window_width = 0
+
+class Message:
+    def __init__(self, text, Y, user):
+        self.text = text
+        self.size = 0
+        self.Y = Y
+        self.user = user
+
 class MessageDelegate(QStyledItemDelegate):
     font = 0
 
@@ -60,31 +72,60 @@ class MessageDelegate(QStyledItemDelegate):
         self.font = font
 
     def paint(self, painter, option, index):
-        user, text = index.model().data(index, Qt.DisplayRole)
-        size = self.sizeHint(option, index)
-        rect = QRect(option.rect.right() - size.width() - 20, option.rect.y(), size.width(), size.height())
+        msg = index.model().data(index, Qt.DisplayRole)
+        field = QRect(option.rect)
+        field = field.marginsRemoved(Defines.TEXT_PADDING)
+        doc = QTextDocument(msg.text)
+        doc.setDocumentMargin(0)
+        opt = QTextOption()
+        opt.setWrapMode(opt.WrapAtWordBoundaryOrAnywhere)
+        doc.setDefaultTextOption(opt)
+        doc.setDefaultFont(self.font)
+        doc.setTextWidth(field.size().width() - 20)
+        field.setHeight(int(doc.size().height()))
+        field.setWidth(int(doc.idealWidth()))
+        field = field.marginsAdded(Defines.TEXT_PADDING)
+        if msg.user == Defines.USER_ME:
+            rect = QRect(option.rect.right() - field.size().width() - 20, msg.Y, field.size().width(), field.size().height())
+        else:
+            rect = QRect(20, msg.Y, field.size().width(), field.size().height())
         bubblerect = rect.marginsRemoved(Defines.BUBBLE_PADDING)
         textrect = rect.marginsRemoved(Defines.TEXT_PADDING)
         painter.setPen(Qt.NoPen)
-        color = QColor(Defines.BUBBLE_COLORS[user])
+        color = QColor(Defines.BUBBLE_COLORS[msg.user])
         painter.setBrush(color)
         painter.drawRoundedRect(bubblerect, 10, 10)
-        if user == Defines.USER_ME:
+        if msg.user == Defines.USER_ME:
             p1 = bubblerect.topRight()
         else:
             p1 = bubblerect.topLeft()
         painter.drawPolygon(p1 + QPoint(-20, 0), p1 + QPoint(20, 0), p1 + QPoint(0, 15))
         painter.setPen(Qt.black)
         painter.setFont(self.font)
-        painter.drawText(textrect, Qt.TextWordWrap, text)
+        painter.translate(textrect.x(), textrect.y())
+        textrectf = QRectF(textrect)
+        textrectf.moveTo(0, 0)
+        doc.drawContents(painter, textrectf)
+        painter.translate(-textrect.x(), -textrect.y())
 
     def sizeHint(self, option, index):
-        _, text = index.model().data(index, Qt.DisplayRole)
-        metrics = QFontMetrics(self.font)
-        rect = option.rect.marginsRemoved(Defines.TEXT_PADDING)
-        rect = metrics.boundingRect(rect, Qt.TextWordWrap, text)
-        rect = rect.marginsAdded(Defines.TEXT_PADDING)
-        return rect.size()
+        global window_width
+        msg = index.model().data(index, Qt.DisplayRole)
+        field = QRect(option.rect)
+        field.setWidth(window_width - 30)
+        field = field.marginsRemoved(Defines.TEXT_PADDING)
+        doc = QTextDocument(msg.text)
+        doc.setDocumentMargin(0)
+        opt = QTextOption()
+        opt.setWrapMode(opt.WrapAtWordBoundaryOrAnywhere)
+        doc.setDefaultTextOption(opt)
+        doc.setDefaultFont(self.font)
+        doc.setTextWidth(field.size().width() - 20)
+        field.setHeight(int(doc.size().height()))
+        field.setWidth(int(doc.idealWidth()))
+        field = field.marginsAdded(Defines.TEXT_PADDING)
+        index.model().setSize(index, field.size().height())
+        return QSize(0, field.size().height())
 
 
 class MessageModel(QAbstractListModel):
@@ -99,11 +140,23 @@ class MessageModel(QAbstractListModel):
     def rowCount(self, index):
         return len(self.messages)
 
-    def add_message(self, who, text):
-        if text:
-            self.messages.append((who, text))
-            self.layoutChanged.emit()
+    def setSize(self, index, size):
+        if (size != self.messages[index.row()].size):
+            self.messages[index.row()].size = size
+            curY = self.messages[index.row()].Y + size
+            for i in range(index.row() + 1, len(self.messages)):
+                self.messages[i].Y = curY
+                curY += self.messages[i].size
 
+    def add_message(self, text, user):
+        if text:
+            length = len(self.messages)
+            Y = 0
+            if length != 0:
+                Y = self.messages[length - 1].size + self.messages[length - 1].Y
+            cur = len(self.messages)
+            self.messages.append(Message(text, Y, user))
+            self.layoutChanged.emit()
 
 class PicButton(QAbstractButton):
     def __init__(self, pixmap, pixmap_hover, pixmap_pressed, parent=None):
@@ -136,19 +189,20 @@ class MainWindow(QMainWindow):
     font = 0
 
     def __init__(self):
+        global window_width
         super(MainWindow, self).__init__()
         id = QFontDatabase.addApplicationFont(Defines.FONT_FILE)
         family = QFontDatabase.applicationFontFamilies(id)[0]
         self.font = QFont(family, Defines.FONT_SIZE)
         self.setMinimumSize(int(QApplication.primaryScreen().size().width() * 0.1), int(QApplication.primaryScreen().size().height() * 0.2))
         self.resize(int(QApplication.primaryScreen().size().width() * 0.3), int(QApplication.primaryScreen().size().height() * 0.5))
+        window_width = int(QApplication.primaryScreen().size().width() * 0.3)
         main_layout = QVBoxLayout()
         send_layout = QHBoxLayout()
         self.send_input = QTextEdit()
         self.send_input.setPlaceholderText(Defines.PLACEHOLDER_TEXT)
         self.send_input.setFont(self.font)
         self.send_input.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
-        self.message_resize()
         self.send_input.textChanged.connect(self.message_resize)
         self.send_input.installEventFilter(self)
         self.send_btn = PicButton(QPixmap(Defines.SEND_IDLE_IMG), QPixmap(Defines.SEND_HOVER_IMG), QPixmap(Defines.SEND_PRESS_IMG))
@@ -157,7 +211,7 @@ class MainWindow(QMainWindow):
         #self.messages.setStyleSheet("background-image: url(:/img/back.jpg);");
         self.model = MessageModel()
         self.messages.setModel(self.model)
-        self.send_btn.pressed.connect(self.message_to)
+        self.send_btn.pressed.connect(self.message_from)
         main_layout.addWidget(self.messages)
         send_layout.addWidget(self.send_input)
         send_layout.addWidget(self.send_btn, 0, Qt.AlignBottom)
@@ -167,14 +221,17 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.w)
         self.setWindowTitle(Defines.APP_NAME)
         self.setWindowIcon(QIcon(Defines.LOGO_IMG))
+        self.message_resize()
 
     def message_to(self):
-        self.model.add_message(Defines.USER_ME, self.send_input.toPlainText())
+        self.model.add_message(self.send_input.toPlainText(), Defines.USER_ME)
         self.messages.scrollToBottom()
         self.send_input.clear()
 
     def message_from(self):
-        self.model.add_message(USER_THEM, self.send_input.toPlainText())
+        self.model.add_message(self.send_input.toPlainText(), Defines.USER_THEM)
+        self.messages.scrollToBottom()
+        self.send_input.clear()
 
     def message_resize(self):
         height = self.send_input.document().documentLayout().documentSize().height()
@@ -188,6 +245,11 @@ class MainWindow(QMainWindow):
         margins = self.send_input.contentsMargins()
         height = fm.lineSpacing() * rowNum + (self.send_input.document().documentMargin() + self.send_input.frameWidth()) * 2 + margins.top() + margins.bottom()
         self.send_input.setFixedHeight(int(height))
+
+    def resizeEvent(self, event):
+        global window_width
+        super(MainWindow, self).resizeEvent(event)
+        window_width = self.size().width()
 
     def eventFilter(self, widget, event):
         if event.type() == QEvent.KeyPress:
